a62e704c1138256890e05223516b33332f4de9c1
vietbuii <124328954+vietbuii@users.noreply.github.com>
2023-02-28 16:02:17 +0700

    del

--- CV/js/main.js
+++
@@ -1,7 +0,0 @@
-//menu
-const menu = document.querySelector(".navbar_links")
-const menuButton = document.querySelector(".navbar_icons")
-
-menuButton.addEventListener('click', ()=>{
-    menu.classList.toggle('navbar_open')
-});
